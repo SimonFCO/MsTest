255f23f7420d0ff937b41c1a357e7b9dc4f29691
simonFCO <simon.vare6@gmail.com>
2026-05-11 19:31:34 +0200

    Add project files.

---
+++ MsTest.Tests/LibraryTests.cs
@@ -0,0 +1,15 @@
+
+using MsTest;
+
+namespace MsTest.Tests;
+[TestClass]
+public class LibraryTests
+{
+    private LibrarySystem _libSys;
+    [TestMethod]
+
+    // 1.1 Lägg till böcker
+    public void TestMethod1()
+    {
+    }
+}
---
+++ MsTest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LibrarySystem library = new LibrarySystem();
+            UserInterface.DisplayMenu(library);
+        }
+    }
+}
